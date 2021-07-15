9f1c3e7b6fa8e4a6dc948838b24a08222577fe38
RudinP <65944851+RudinP@users.noreply.github.com>
2021-07-15 16:12:59 +0900

    Add Character Animation&Script

---
+++ WaitSomeleading_Game/Assets/Script/Mover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Animator _animator;
+    private Vector2 _pos;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _pos.x = Input.GetAxisRaw("Horizontal");
+            _pos.y = 0;
+
+            _animator.SetBool("Walking", true);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _pos.x = 0;
+            _pos.y = Input.GetAxisRaw("Vertical");
+
+            _animator.SetBool("Walking", true);
+        }
+        else
+        {
+            _animator.SetBool("Walking", false);
+        }
+
+        _animator.SetFloat("x", _pos.x);
+        _animator.SetFloat("y", _pos.y);
+    }
+}
